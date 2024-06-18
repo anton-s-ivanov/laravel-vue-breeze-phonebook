89efcd50477d4e654285b878d4cd0133bcbc9220
anton-s-ivanov <a.ivanov@kvant.app>
2024-06-18 21:25:04 +0300

    api + swagger - done

--- app/Http/Controllers/Api/Phonebook/ContactController.php
+++ app/Http/Controllers/Api/Phonebook/ContactController.php
@@ -8,31 +8,80 @@ use App\Http\Requests\ContactRequest;
 use App\Http\Resources\ContactResource;
 use App\Models\Contact;
 use App\Repositories\Interfaces\ContactRepositoryInterface;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Phonebook API",
+ *     description="API documentation for the Phonebook"
+ * )
+ * @OA\Tag(
+ *     name="Contacts",
+ *     description="API Endpoints for Contacts"
+ * )
+ */
 class ContactController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @OA\Get(
+     *     path="/api/contacts",
+     *     tags={"Contacts"},
+     *     summary="Get list of contacts",
+     *     description="Returns list of contacts",
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(type="array", @OA\Items(ref="#/components/schemas/Contact"))
+     *     )
+     * )
      */
     public function index()
     {
         return ContactResource::collection(Auth::user()->contacts);
     }
 
-    /**
-     * Store a newly created resource in storage.
+   /**
+     * @OA\Post(
+     *     path="/api/contacts",
+     *     tags={"Contacts"},
+     *     summary="Create a new contact",
+     *     description="Creates a new contact",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/Contact")
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Contact created"
+     *     )
+     * )
      */
     public function store(ContactRequest $request)
     {
         $data = $request->validated();
         $data['user_id'] = Auth::id(); 
-        ContactStoreAction::store($data);
+        return ContactStoreAction::store($data);
     }
 
     /**
-     * Display the specified resource.
+     * @OA\Get(
+     *     path="/api/contacts/{contact_id}",
+     *     tags={"Contacts"},
+     *     summary="Get a specific contact",
+     *     description="Returns a single contact",
+     *     @OA\Parameter(
+     *         name="contact_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(ref="#/components/schemas/Contact")
+     *     )
+     * )
      */
     public function show(ContactRepositoryInterface $contactRepository, string $id)
     {
@@ -40,20 +89,54 @@ class ContactController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * @OA\Put(
+     *     path="/api/contacts/{contact_id}",
+     *     tags={"Contacts"},
+     *     summary="Update a specific contact",
+     *     description="Updates a single contact",
+     *     @OA\Parameter(
+     *         name="contact_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(ref="#/components/schemas/Contact")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Contact updated"
+     *     )
+     * )
      */
     public function update(ContactRequest $request, string $id)
     {
         $data = $request->validated();
         $data['user_id'] = Auth::id(); 
-        ContactStoreAction::update($id, $data);
+        return ContactStoreAction::update($id, $data);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @OA\Delete(
+     *     path="/api/contacts/{contact_id}",
+     *     tags={"Contacts"},
+     *     summary="Delete a specific contact",
+     *     description="Deletes a single contact",
+     *     @OA\Parameter(
+     *         name="contact_id",
+     *         in="path",
+     *         required=true,
+     *         @OA\Schema(type="integer")
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Contact deleted"
+     *     )
+     * )
      */
     public function destroy(string $id)
     {
-        Contact::find($id)->delete();
+        return Contact::find($id)->delete();
     }
 }
--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,6 +5,41 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Contact",
+ *     type="object",
+ *     title="Contact",
+ *     required={"name", "phone"},
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         format="int64",
+ *         example=1
+ *     ),
+ *      @OA\Property(
+ *         property="user_id",
+ *         type="integer",
+ *         format="int64",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         example="John Doe"
+ *     ),
+ *     @OA\Property(
+ *         property="phone",
+ *         type="integer",
+ *         example="1234567890"
+ *     ),
+ *      @OA\Property(
+ *         property="marked",
+ *         type="boolean",
+ *         example="false"
+ *     ),
+ * )
+ */
 class Contact extends Model
 {
     use HasFactory;
